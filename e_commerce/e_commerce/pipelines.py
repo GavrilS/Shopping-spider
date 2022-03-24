91afc58bce6b3b3ac4cb3aace6d306723cd90a69
GavrilS <gavr1lll@abv.bg>
2022-03-24 19:20:48 +0000

    Enabled pipeline settings in project and added connection method to postgresql db in pipeline

--- e_commerce/e_commerce/pipelines.py
+++ e_commerce/e_commerce/pipelines.py
@@ -5,9 +5,27 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
+# from itemadapter import ItemAdapter
+import psycopg2
 
 class ECommercePipeline:
+
+    def __init__(self) -> None:
+        self.create_connection()
+
+    def create_connection(self):
+        try:
+            self.conn = psycopg2.connect(
+                database='',user='',password='',host='',port=''
+            )
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            print("Error connecting to db ^^^^^^^^^^^^^^^^^^^^")
+            print(e)
+            exit()
+
+    def close_connection(self):
+        self.conn.close()
+
     def process_item(self, item, spider):
         return item
